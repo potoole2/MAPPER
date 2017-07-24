9b6ed745f98f128f368c22cbee1473e4e785137f
potoole2 <potoole2@nd.edu>
2017-07-24 12:26:17 -0400

    Update Optimization.m

--- Stitching/Optimization.m
+++ Stitching/Optimization.m
@@ -2,7 +2,7 @@ function Optimization(M,N,tempDirectory,channels,numembs)
 
 
 
-
+% Find tile coordinates
 registration = importdata([tempDirectory 'TileConfiguration.registered.txt']);
 registration = extractBetween(registration(4:size(registration)),'(',')');
 for i = 1:size(registration)
@@ -15,8 +15,9 @@ tileCoordinates = round(tileCoordinates);
 tileCoordinates(:,1) = tileCoordinates(:,1) + (1-min(tileCoordinates(:,1)));
 tileCoordinates(:,2) = tileCoordinates(:,2) + (1-min(tileCoordinates(:,2)));
 
+% Generate cell array with raw tiles, coordinates of top left tile corner,
+% and coordinates of bottom right tile corner
 files = dir(tempDirectory);
-
 for i = 1:M*N
     for j = 1:length(channels)
         tiles{i,1}(:,:,j) = imread([tempDirectory files(i+2).name],j);
@@ -24,49 +25,51 @@ for i = 1:M*N
     tiles{i,2} = tileCoordinates(i,:);
     tiles{i,3} = tiles{i,2} + size(tiles{i,1}(:,:,1)) - 1;
 end
+
+% Generate struct that contains information about which tiles overlap
 overlapsize=0;
 for i = 1:M*N
     for j = 1:M*N
         if i == j
-            overlaps(i).reg(j) = 0;
+            overlaps(i).registration(j) = 0;
         elseif ((tiles{j,2}(1) >= tiles{i,2}(1)) && ((tiles{j,2}(1) <= tiles{i,3}(1))) || ((tiles{j,3}(1) >= tiles{i,2}(1)) && ((tiles{j,3}(1) <= tiles{i,3}(1)))))...
                 && ((tiles{j,2}(2) >= tiles{i,2}(2)) && ((tiles{j,2}(2) <= tiles{i,3}(2))) || ((tiles{j,3}(2) >= tiles{i,2}(2)) && ((tiles{j,3}(2) <= tiles{i,3}(2)))))
-            overlaps(i).reg(j) = 1;
+            overlaps(i).registration(j) = 1;
         else
-            overlaps(i).reg(j) = 0;
+            overlaps(i).registration(j) = 0;
         end
     end
-    overlaps(i).reg = find(overlaps(i).reg);
-    if length(overlaps(i).reg) > overlapsize
-        overlapsize = length(overlaps(i).reg);
+    overlaps(i).registration = find(overlaps(i).registration);
+    if length(overlaps(i).registration) > overlapsize
+        overlapsize = length(overlaps(i).registration);
     end
 end
 
 
-
+% Generate arrays of overlaps
 overlaps1 = zeros(M*N,length(channels),20,20,overlapsize);
 overlaps2 = zeros(M*N,length(channels),20,20,overlapsize);
 for i= 1:M*N
-    for k = 1:length(overlaps(i).reg)
-        if tiles{overlaps(i).reg(k),2}(1) <= tiles{i,2}(1)
+    for k = 1:length(overlaps(i).registration)
+        if tiles{overlaps(i).registration(k),2}(1) <= tiles{i,2}(1)
             i1 = 1;
-        elseif tiles{overlaps(i).reg(k),2}(1) > tiles{i,2}(1)
-            i1 = tiles{overlaps(i).reg(k),2}(1) - tiles{i,2}(1) + 1;
+        elseif tiles{overlaps(i).registration(k),2}(1) > tiles{i,2}(1)
+            i1 = tiles{overlaps(i).registration(k),2}(1) - tiles{i,2}(1) + 1;
         end
-        if tiles{overlaps(i).reg(k),2}(2) <= tiles{i,2}(2)
+        if tiles{overlaps(i).registration(k),2}(2) <= tiles{i,2}(2)
             j1 = 1;
-        elseif tiles{overlaps(i).reg(k),2}(2) > tiles{i,2}(2)
-            j1 = tiles{overlaps(i).reg(k),2}(2) - tiles{i,2}(2) + 1;
+        elseif tiles{overlaps(i).registration(k),2}(2) > tiles{i,2}(2)
+            j1 = tiles{overlaps(i).registration(k),2}(2) - tiles{i,2}(2) + 1;
         end
-        if tiles{overlaps(i).reg(k),3}(1) >= tiles{i,3}(1)
+        if tiles{overlaps(i).registration(k),3}(1) >= tiles{i,3}(1)
             i2 = tiles{i,3}(1) - tiles{i,2}(1) + 1;
-        elseif tiles{overlaps(i).reg(k),3}(1) < tiles{i,3}(1)
-            i2 = tiles{overlaps(i).reg(k),3}(1) - tiles{i,2}(1) + 1;
+        elseif tiles{overlaps(i).registration(k),3}(1) < tiles{i,3}(1)
+            i2 = tiles{overlaps(i).registration(k),3}(1) - tiles{i,2}(1) + 1;
         end
-        if tiles{overlaps(i).reg(k),3}(2) >= tiles{i,3}(2)
+        if tiles{overlaps(i).registration(k),3}(2) >= tiles{i,3}(2)
             j2 = tiles{i,3}(2) - tiles{i,2}(2) + 1;
-        elseif tiles{overlaps(i).reg(k),3}(2) < tiles{i,3}(2)
-            j2 = tiles{overlaps(i).reg(k),3}(2) - tiles{i,2}(2) + 1;
+        elseif tiles{overlaps(i).registration(k),3}(2) < tiles{i,3}(2)
+            j2 = tiles{overlaps(i).registration(k),3}(2) - tiles{i,2}(2) + 1;
         end
         for h = 1:length(channels)
             overlaps1(i,h,:,:,k) = imresize(double(tiles{i,1}(i1:i2,j1:j2,h)),[20 20],'bicubic');
@@ -74,19 +77,18 @@ for i= 1:M*N
     end
 end
 for i=1:M*N
-    for j = 1: length(overlaps(i).reg)
-        overlaps2(i,:,:,:,j) = overlaps1(overlaps(i).reg(j),:,:,:,find(overlaps(overlaps(i).reg(j)).reg == i));
+    for j = 1: length(overlaps(i).registration)
+        overlaps2(i,:,:,:,j) = overlaps1(overlaps(i).registration(j),:,:,:,find(overlaps(overlaps(i).registration(j)).registration == i));
     end
 end
 
 
-
+% Find a and b parameters for optimization by minimizing overlap residuals
 chan = length(channels);
-
-
 x0=[ones(M*N,length(channels)) zeros(M*N,length(channels))];
 x = fminsearch(@OverlapResiduals,x0,optimset('MaxFunEvals',100),overlaps,overlaps1,overlaps2,chan);
 
+% Apply and b parameters to raw tiles and save optimized tiles
 for i = 1:M*N
     for j = 1:length(channels)
         tiles{i,1}(:,:,j) = tiles{i,1}(:,:,j).*x(i,j) + x(i,j + chan);
