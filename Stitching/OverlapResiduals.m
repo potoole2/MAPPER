6327fea4d5b00945db5d1a82395980e72f90fd7d
potoole2 <potoole2@nd.edu>
2017-07-27 10:55:58 -0400

    Update OverlapResiduals.m

--- Stitching/OverlapResiduals.m
+++ Stitching/OverlapResiduals.m
@@ -1,23 +1,26 @@
-function r=OverlapResiduals(x0,overlaps,overlaps1,overlaps2,chan,M,N,xres,yres,means)
+function r=OverlapResiduals(x0,overlaps,overlaps1,overlaps2,numchannels,numtiles,M,N,xres,yres,channelmeans)
 
-x0((round(N/2)-1)*M+round(M/2),1:chan)=means;
-x0((round(N/2)-1)*M+round(M/2),chan+1:2*chan)=0;
+x0((round(N/2)-1)*M+round(M/2),1:numchannels)=channelmeans;
+x0((round(N/2)-1)*M+round(M/2),numchannels+1:2*numchannels)=0;
 
-    
+numoverlaps=size(overlaps1,5);
+x1=zeros(numtiles,2*numchannels,1,1,numoverlaps);
+scale = zeros(numtiles,numchannels);
+
     % Optimize overlap array with input parameters
-    opoverlaps1=x0(:,1:chan).*overlaps1+x0(:,chan+1:2*chan);
-    for j=1:4
-        for i = 1:36
+    opoverlaps1=x0(:,1:numchannels).*overlaps1+x0(:,numchannels+1:2*numchannels);
+    for j=1:numchannels
+        for i = 1:numtiles
             for k = 1:length(overlaps(i).registration)
                 x1(i,j,1,1,k)=x0(overlaps(i).registration(k),j);
-                x1(i,j+chan,1,1,k)=x0(overlaps(i).registration(k),j+chan);
+                x1(i,j+numchannels,1,1,k)=x0(overlaps(i).registration(k),j+numchannels);
                 
-%                 scale2(1:36,j)=mean(mean(opoverlaps1(:,j,:)));
+
             end
         end
-        scale(1:36,j)=mean(mean(opoverlaps1(:,j,:)));
+        scale(1:numtiles,j)=mean(mean(opoverlaps1(:,j,:)));
     end
-    scale2=repmat(scale(:),xres*yres*9,1);
-    opoverlaps2=x1(:,1:chan,:,:,:).*overlaps2+x1(:,chan+1:2*chan,:,:,:);
+    scale2=repmat(scale(:),xres*yres*numoverlaps,1);
+    opoverlaps2=x1(:,1:numchannels,:,:,:).*overlaps2+x1(:,numchannels+1:2*numchannels,:,:,:);
     % Calculate overlap residuals for input parameters
     r= sum(((opoverlaps1(:)-opoverlaps2(:))./scale2).^2);
